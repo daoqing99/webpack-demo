c6d13e94d8aa2ac13ebe47a90d9f0631c0f4e8a3
daoqing99 <daoqing99@163.com>
2020-04-26 11:58:06 +0800

    first commit

---
+++ content.js
@@ -0,0 +1,7 @@
+function Content() {
+  var dom = document.getElementById("root");
+  var content = document.createElement('div')
+  content.innerHTML = 'content'
+  dom.append(content)
+}
+export default Content;
---
+++ index.js
@@ -0,0 +1,7 @@
+import Header from "./header";
+import Sidebar from "./sidebar";
+import Content from "./content";
+
+new Header();
+new Content();
+new Sidebar();
